1b46a5d715d0098b85a8a1c124062041481303dc
Dylan Kirby <dylankirbydev@outlook.com>
2021-11-07 15:03:28 -0800

    finalize and refactor email client tests

--- src/email_client.rs
+++ src/email_client.rs
@@ -12,58 +12,70 @@ pub struct EmailClient {
 
 #[derive(serde::Serialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct SendEmailRequestData {
-	text_body: String,
-	html_body: String,
-	subject: String,
-	to: String,
-	from: String
+pub struct SendEmailRequestData<'a> {
+	text_body: &'a str,
+	html_body: &'a str,
+	subject: &'a str,
+	to: &'a str,
+	from: &'a str
 }
 
 impl EmailClient {
 	pub fn new(base_url: String, sender: SubscriberEmail, authorization_token: String) -> Self {
+		let http_client = reqwest::Client::builder()
+			.timeout(std::time::Duration::from_secs(10))
+			.build()
+			.unwrap();
+
 		Self {
-			client: reqwest::Client::new(),
+			client: http_client,
 			sender: sender,
 			base_url: base_url,
 			authorization_token: authorization_token
 		}
 	}
 
-	pub async fn send_email(&self, recipient: SubscriberEmail, subject: &str, html_content: &str, text_content: &str) -> Result<reqwest::Response, reqwest::Error> {
-		let post_data = self.construct_request_json(recipient, &subject, &html_content, &text_content);
-		let res = self.client
-			.post(&self.construct_url())
-			.header("X-Postmark-Server-Token", &self.authorization_token)
-			.json(&post_data)
-			.send()
-			.await?;
+	pub async fn send_email(&self, recipient: SubscriberEmail, subject: &str, html_content: &str, text_content: &str) -> Result<(), reqwest::Error> {
+		let request_body = SendEmailRequestData {
+			text_body: text_content,
+			html_body: html_content,
+			subject: subject,
+			to: recipient.as_ref(),
+			from: self.sender.as_ref()
+		};
 
-		Ok(res)
+		let url = self.construct_url();
+
+		self.client
+			.post(&url)
+			.header("X-Postmark-Server-Token", &self.authorization_token)
+			.json(&request_body)
+			.send()
+			.await?
+			.error_for_status()?;
+
+		Ok(())
 	}
 
 	pub fn construct_url(&self) -> String {
 		return format!("{}/email", self.base_url)
 	}
 
-	pub fn construct_request_json(&self, recipient: SubscriberEmail, subject: &str, html_content: &str, text_content: &str) -> SendEmailRequestData {
-		SendEmailRequestData {
-			text_body: text_content.to_string(),
-			html_body: html_content.to_string(),
-			subject: subject.to_string(),
-			to: recipient.as_ref().to_owned(),
-			from: self.sender.as_ref().to_owned()
-		}
-	}
 }
 
 #[cfg(test)]
 mod tests {
 	use crate::domain::SubscriberEmail;
 	use crate::email_client::EmailClient;
+
+	use fake::faker::internet::en::SafeEmail;
+	use fake::faker::lorem::en::{Paragraph, Sentence};
+	use fake::{Fake, Faker};
+
 	use wiremock::{Match, Request};
 	use wiremock::{Mock, MockServer, ResponseTemplate};
-	use wiremock::matchers::{header_exists, path, method, header};
+	use wiremock::matchers::{header_exists, path, method, header, any};
+	use claim::{assert_ok, assert_err};
 
 	// Define custom matcher for expected body
 	struct SendEmailBodyMatcher;
@@ -79,17 +91,42 @@ mod tests {
 		}
 	}
 
+	fn sender_email() -> SubscriberEmail {
+		SubscriberEmail::parse("test@test.com".to_string()).expect("failed to parse sender email")
+	}
+
+	fn auth_token() -> String {
+		"AB123".to_string()
+	}
+
+	fn email_client(server_uri: String) -> EmailClient {
+		EmailClient::new(server_uri, sender_email(), auth_token())
+	}
+
+	fn subject() -> String {
+		Paragraph(1..2).fake()
+	}
+
+	fn html_content() -> String {
+		content()
+	}
+
+	fn content() -> String {
+		Paragraph(1..10).fake()
+	}
+
+	fn recipient() -> SubscriberEmail {
+		SubscriberEmail::parse(SafeEmail().fake()).expect("failed to parse sender email")
+	}
+
+
 
 	#[tokio::test]
 	async fn send_email_fires_request_to_base_url() {
-		// todo - use fakes for values
 		let mock_server = MockServer::start().await;
-
 		let server_uri = mock_server.uri();
-		let sender_email = SubscriberEmail::parse("test@test.com".to_string()).expect("failed to parse sender email");
-		let auth_token = "AB123".to_string();
 
-		let email_client = EmailClient::new(server_uri, sender_email, auth_token);
+		let email_client = email_client(server_uri);
 
 		Mock::given(method("POST"))
 		.and(path("/email"))
@@ -101,15 +138,68 @@ mod tests {
 		.mount(&mock_server)
 		.await;
 
-		let subject = "Test Email";
-		let html_content = "<h1> Test </h1>";
-		let content = "Test";
-		let recipient = SubscriberEmail::parse("test@gmail.com".to_string()).expect("failed to parse sender email");
+		let _ = email_client.send_email(recipient(), &subject(), &html_content(), &content())
+			.await;
+	}
 
-		let res = email_client.send_email(recipient, &subject, &html_content, &content)
-			.await
-			.unwrap();
+	#[tokio::test]
+	async fn send_email_succeeds_on_200_return() {
+		let mock_server = MockServer::start().await;
+		let server_uri = mock_server.uri();
 
-		assert_eq!(res.status(), 200)
+		let email_client = email_client(server_uri);
+
+		Mock::given(any())
+		.respond_with(ResponseTemplate::new(200))
+		.expect(1)
+		.mount(&mock_server)
+		.await;
+
+
+		let res = email_client.send_email(recipient(), &subject(), &html_content(), &content())
+			.await;
+
+		assert_ok!(res)
+	}
+
+	#[tokio::test]
+	async fn send_email_failed_on_500() {
+		let mock_server = MockServer::start().await;
+		let server_uri = mock_server.uri();
+
+		let email_client = email_client(server_uri);
+
+		Mock::given(any())
+		.respond_with(ResponseTemplate::new(500))
+		.expect(1)
+		.mount(&mock_server)
+		.await;
+
+		let res = email_client.send_email(recipient(), &subject(), &html_content(), &content())
+			.await;
+
+		assert_err!(res);
+	}
+
+	#[tokio::test]
+	async fn send_email_on_timeout() {
+		let mock_server = MockServer::start().await;
+		let server_uri = mock_server.uri();
+
+		let email_client = email_client(server_uri);
+
+		let response = ResponseTemplate::new(200)
+			.set_delay(std::time::Duration::from_secs(180));
+
+		Mock::given(any())
+		.respond_with(response)
+		.expect(1)
+		.mount(&mock_server)
+		.await;
+
+		let res = email_client.send_email(recipient(), &subject(), &html_content(), &content())
+			.await;
+
+		assert_err!(res);
 	}
 }
