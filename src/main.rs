a7c7cc1054e4e4a7c9f7f363933eaadc566c3e18
Dylan Kirby <dylankirbydev@outlook.com>
2021-11-07 16:25:50 -0800

    abstract startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use std::net::TcpListener;
-
-use sqlx::postgres::PgPoolOptions;
-
-use zero2prod::startup::run;
+use zero2prod::startup::{run, pre_run_build};
 use zero2prod::configurations::get_configurations;
 use zero2prod::telemetry::{get_tracing_subscriber, init_tracing_subscriber};
-use zero2prod::email_client::EmailClient;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -13,23 +8,7 @@ async fn main() -> std::io::Result<()> {
 	init_tracing_subscriber(subscriber);
 
 	let configs = get_configurations().expect("Unable to load configs");
-	let db_connection_url = configs.database.connection_url();
-	let db_pool = PgPoolOptions::new()
-		.connect_timeout(std::time::Duration::from_secs(2))
-		.connect(&db_connection_url)
-		.await
-		.expect("Failed to connect to Postgres");
+	let (listener, email_client, db_pool) = pre_run_build(configs).await?;
 
-	let sender_email = configs.email_client.get_sender_email()
-		.expect("Failed to parse sender email, seems invalid");
-
-	let email_client = EmailClient::new(
-		configs.email_client.base_url,
-		sender_email,
-		configs.email_client.authorization_token
-	);
-
-	let application_address = format!("{}:{}", configs.application.host, configs.application.port);
-	let listener = TcpListener::bind(application_address)?;
     run(listener, db_pool, email_client)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,15 @@
 use std::net::TcpListener;
 
 use sqlx::{PgPool};
+use sqlx::postgres::PgPoolOptions;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use actix_web::web::Data;
 use tracing_actix_web::TracingLogger;
 
+use crate::configurations::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscriptions_post};
-
 
 
 pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) -> Result<Server, std::io::Error> {
@@ -26,4 +27,27 @@ pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) ->
     .run();
 
     Ok(server)
+}
+
+pub async fn pre_run_build(configs: Settings) -> Result<(TcpListener, EmailClient, PgPool), std::io::Error>  {
+    let db_connection_url = configs.database.connection_url();
+    let db_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&db_connection_url)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let sender_email = configs.email_client.get_sender_email()
+        .expect("Failed to parse sender email, seems invalid");
+
+    let email_client = EmailClient::new(
+        configs.email_client.base_url,
+        sender_email,
+        configs.email_client.authorization_token
+    );
+
+    let application_address = format!("{}:{}", configs.application.host, configs.application.port);
+    let listener = TcpListener::bind(application_address)?;
+
+    return Ok((listener, email_client, db_pool))
 }
